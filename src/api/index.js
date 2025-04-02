5478181cc009e2a33f972caf45d1bd1169d2c4a5
Edi Cziszter <eduard.cziszter@student.upt.ro>
2025-04-02 19:42:27 +0300

    forgot these

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://dev.ccoc.edicz.com/api/';
-export const DOMAIN = 'https://dev.ccoc.edicz.com';
+const API_URL = (process.env.REACT_APP_API_URL || 'http://localhost:6901') + '/api';
+export const DOMAIN = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 const api = axios.create({
     baseURL: API_URL,
